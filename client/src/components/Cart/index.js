66d3a0f0d2741e741dae87403cd4e5b24fb2fdc5
burcar <zacharyburcar@gmail.com>
2022-03-04 10:45:19 -0500

    added idb functionality

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -3,14 +3,40 @@ import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import '.style.css';
 
+import { useStoreContext } from '../../utils/GlobalState';
+import { TOGGLE_CART } from '../../utils/actions';
+
 const Cart = () => {
+    const [state, dispatch] = useStoreContext();
+
+    function toggleCart() {
+        dispatch({ type: TOGGLE_CART });
+    };
+
+    function calculateTotal() {
+        let sum = 0;
+        state.cart.forEach(item => {
+            sum += item.price * item.purchaseQuantity;
+        });
+        return sum.toFixed(2)
+    }
+    // return a smaller div if the cart is closed
+    if (!state.cartOpen) {
+        return (
+            <div className='cart-closed' onClick={toggleCart}>
+                <span
+                role='img'
+                aria-label='trash'>🛒</span>
+            </div>
+        )
+    }
     return (
         <div className='cart'>
-            <div className='close'>[close]</div>
+            <div className='close' onClick={toggleCart}>[close]</div>
             <h2>Shopping Cart</h2>
             <div>
                 <div className='flex-row space-between'>
-                    <strong>Total: $0
+                    <strong>Total: ${calculateTotal()}
                     </strong>
                     {
                         Auth.loggedIn() ?
